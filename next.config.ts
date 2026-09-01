96f0068fa208d4cf97ea86bb0a31b14649ea399c
Mahmud Adegboyega <mahmud.adegboyega@gmail.com>
2026-09-01 17:44:16 +0100

    whip:bang

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,11 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async rewrites() {
+    return [
+      {source: "/api/:path*", destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`}
+    ]
+  }
 };
 
 export default nextConfig;
